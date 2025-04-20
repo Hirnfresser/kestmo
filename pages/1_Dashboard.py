b9fb93041d51820900891a570582d3ed5b5a0833
Hirnfresser <sara@freaky.ch>
2025-04-20 19:21:30 +0200

    restliche Semester in Dashboard-Dropdown hizugefügt

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 st.title('Dashboard')
 
 if 'semester' not in st.session_state:
-    semester = st.selectbox('Wähle das Semester', ['Herbstsemester 1', 'Frühlingssemester 1'])
+    semester = st.selectbox('Wähle das Semester', ['Herbstsemester 1', 'Frühlingssemester 1', 'Herbstsemester 2', 'Frühlingssemester 2', 'Herbstsemester 3', 'Frühlingssemester 3'])
     st.session_state.semester = semester
 
 else:
-    semester = st.selectbox('Wähle das Semester', ['Herbstsemester 1', 'Frühlingssemester 1'], index=['Herbstsemester 1', 'Frühlingssemester 1'].index(st.session_state.semester))
+    semester = st.selectbox('Wähle das Semester', ['Herbstsemester 1', 'Frühlingssemester 1', 'Herbstsemester 2', 'Frühlingssemester 2', 'Herbstsemester 3', 'Frühlingssemester 3' ], index=['Herbstsemester 1', 'Frühlingssemester 1', 'Herbstsemester 2', 'Frühlingssemester 2', 'Herbstsemester 3', 'Frühlingssemester 3'].index(st.session_state.semester))
     st.session_state.semester = semester
