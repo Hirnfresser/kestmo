c0cd198cbc58881468d8413d2b7cdf747216a38e
Hirnfresser <sara@freaky.ch>
2025-05-07 10:49:02 +0200

    uppsie

--- pages/Login.py
+++ pages/Login.py
@@ -7,14 +7,12 @@ st.title('Hier folgt das Loginfenster')
 
 data_manager = DataManager(fs_protocol='webdav', fs_root_folder="Institution/kestmo_App")  # switch drive 
 
-# Überprüfen, ob ein Benutzer eingeloggt ist
-if st.session_state.get("username"):
-    # load the data from the persistent storage into the session state
-    data_manager.load_user_data(
-        session_state_key='data_df', 
-        file_name='data.csv', 
-        initial_value=pd.DataFrame(),
-        parse_dates=['timestamp']
+# load the data from the persistent storage into the session state
+data_manager.load_user_data(
+    session_state_key='data_df', 
+    file_name='data.csv', 
+    initial_value = pd.DataFrame(),
+    parse_dates = ['timestamp']
     )
 
 # initialize the login manager
